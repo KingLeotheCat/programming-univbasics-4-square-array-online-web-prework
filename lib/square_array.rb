e18626776ba3eb61c7a319365bcf1c8ccd62264e
Lenny Lee <lennylee91@gmail.com>
2019-09-30 03:58:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,9 @@ def sqaure_array(array)
 new_Array = []
 counter=0
 
-while counter < array.length
+while counter < array.length do
+  new_array << array[counter] **2
 counter += 1 n
+end
+return new_array
 end
